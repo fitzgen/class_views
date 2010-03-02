353d1c3918bfad1e6627abae00921a0fa397185e
Nick Fitzgerald <fitzgen@gmail.com>
2010-03-02 13:53:58 -0800

    whitespace

--- views.py
+++ views.py
@@ -15,10 +15,10 @@ def decorate_method_with(function_decorator):
 class GenericView(object):
     """
     Parent class for all generic views.
-    
+
     This was written by jacobian, don't use this directly, use ObjectView.
     """
-            
+
     def __init__(self, **kwargs):
         self._load_config_values(kwargs,
             context_processors = None,
@@ -28,14 +28,14 @@ class GenericView(object):
         )
         if kwargs:
             raise TypeError("__init__() got an unexpected keyword argument '%s'" % iter(kwargs).next())
-    
+
     def __call__(self, request, object=None):
         template = self.get_template(request, object)
         context = self.get_context(request, object)
         mimetype = self.get_mimetype(request, object)
         response = self.get_response(request, object, template, context, mimetype=mimetype)
         return response
-    
+
     def get_template(self, request, obj):
         """
         Get a ``Template`` object for the given request.
@@ -49,28 +49,28 @@ class GenericView(object):
         """
         Return a list of template names to be used for the request. Must return
         a list. May not be called if get_template is overridden.
-        """ 
+        """
         if self.template_name is None:
-            return []                       
+            return []
         elif isinstance(self.template_name, basestring):
             return [self.template_name]
         else:
             return self.template_name
-    
+
     def load_template(self, request, obj, names=[]):
         """
         Load a template, using self.template_loader or the default.
         """
         return self.get_template_loader(request, obj).select_template(names)
-    
+
     def get_template_loader(self, request, obj):
         """
-        Get the template loader to be used for this request. Defaults to 
+        Get the template loader to be used for this request. Defaults to
         ``django.template.loader``.
         """
         import django.template.loader
         return self.template_loader or django.template.loader
-            
+
     def get_context(self, request, obj, context=None):
         """
         Get the context. Must return a Context (or subclass) instance.
@@ -91,13 +91,13 @@ class GenericView(object):
         Get the mimetype to be used for the given request.
         """
         return self.mimetype
-        
+
     def get_response(self, request, obj, template, context, **httpresponse_kwargs):
         """
         Construct an `HttpResponse` object given the template and context.
         """
         return HttpResponse(template.render(context), **httpresponse_kwargs)
-        
+
     def _load_config_values(self, initkwargs, **defaults):
         """
         Set on self some config values possibly taken from __init__, or
@@ -169,7 +169,7 @@ class ObjectView(GenericView):
             return super(ObjectView, self).__call__(request, object)
         except HttpRedirect, e:
             return e.redirect
-        
+
 
 def instantiator(cls, **kwargs):
     """
